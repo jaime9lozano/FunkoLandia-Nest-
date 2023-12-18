03054813d02a71bd892c34223f36ebeac85f20b2
jaime9lozano <jaime9lozano@gmail.com>
2023-12-18 17:50:15 +0100

    errores

--- src/funko/dto/update-funko.dto.ts
+++ src/funko/dto/update-funko.dto.ts
@@ -1,36 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateFunkoDto } from './create-funko.dto';
-import {
-  IsBoolean,
-  IsEnum,
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-  IsUrl,
-  Min,
-} from 'class-validator';
-import { Categoria } from '../entities/funko.entity';
+import { IsBoolean } from 'class-validator';
 
 export class UpdateFunkoDto extends PartialType(CreateFunkoDto) {
-  @IsString({ message: 'El nombre debe ser un string' })
-  @IsNotEmpty({ message: 'El nombre no puede estar vacío' })
-  readonly nombre: string;
-  @IsNotEmpty({ message: 'El precio no puede estar vacío' })
-  @IsNumber()
-  @Min(0, { message: 'El precio no puede estar negativo' })
-  readonly precio: number;
-  @IsNotEmpty({ message: 'La cantidad no puede estar vacía' })
-  @Min(0, { message: 'La cantidad no puede estar negativo' })
-  @IsInt({ message: 'La cantidad debe ser un entero' })
-  readonly cantidad: number;
-  @IsUrl()
-  @IsNotEmpty({ message: 'La imagen no puede estar vacía' })
-  readonly imagen: string;
-  @IsNotEmpty({ message: 'La categoria no puede estar vacía' })
-  @IsEnum(Categoria, {
-    message: 'La categoria debe ser disney,marvel,superheroes,otros',
-  })
   @IsBoolean({ message: 'is_deleted tiene que ser un boolean' })
   readonly is_deleted: boolean = false;
 }
