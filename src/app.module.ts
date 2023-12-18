65b13f705fae8d4573b091ca71e93781bfe06d1a
jaime9lozano <jaime9lozano@gmail.com>
2023-12-18 20:32:05 +0100

    categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { FunkoModule } from './funko/funko.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
-  imports: [FunkoModule],
+  imports: [
+    FunkoModule,
+    // Configuración de la conexión a la base de datos a PostgreSQL
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Tipo de base de datos
+      host: 'localhost', // Dirección del servidor
+      port: 5432, // Puerto del servidor
+      username: 'admin', // Nombre de usuario
+      password: 'adminPassword123', // Contraseña de usuario
+      database: 'NEST_DB', // Nombre de la base de datos
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`], // Entidades de la base de datos (buscar archivos con extensión .entity.ts o .entity.js)
+      synchronize: true, // Sincronizar la base de datos
+    }),
+    CategoriaModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/funko/dto/update-funko.dto.ts
+++ src/funko/dto/update-funko.dto.ts
@@ -1,8 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateFunkoDto } from './create-funko.dto';
-import { IsBoolean } from 'class-validator';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { Categoria } from '../entities/funko.entity';
 
 export class UpdateFunkoDto extends PartialType(CreateFunkoDto) {
+  @IsOptional()
+  readonly nombre?: string;
+  @IsOptional()
+  readonly precio?: number;
+  @IsOptional()
+  readonly cantidad?: number;
+  @IsOptional()
+  readonly imagen?: string;
+  @IsOptional()
+  readonly categoria?: Categoria;
+  @IsOptional()
   @IsBoolean({ message: 'is_deleted tiene que ser un boolean' })
   readonly is_deleted: boolean = false;
 }
