b302cc794106218ec60e94b7371515b63d90ff69
jaime9lozano <jaime9lozano@gmail.com>
2024-01-16 20:51:56 +0100

    tienda sql

--- database/tienda.sql
+++ database/tienda.sql
@@ -28,7 +28,21 @@ CREATE TABLE "public"."funkos" (
                                    CONSTRAINT "funkos_pkey" PRIMARY KEY ("id"),
                                    CONSTRAINT "funkos_categoria_id_fkey" FOREIGN KEY ("categoria_id") REFERENCES "public"."categorias" ("id") ON DELETE CASCADE
 );
+-- Inserción de categorías
+INSERT INTO "categorias" ("id", "categoria", "created_at", "updated_at", "is_deleted")
+VALUES
+    ('d69cf3db-b77d-4181-b3cd-5ca8107fb6a9', 'disney', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+    ('6dbcbf5e-8e1c-47cc-8578-7b0a33ebc154', 'marvel', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+    ('9def16db-362b-44c4-9fc9-77117758b5b0', 'superheroe', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+    ('8c5c06ba-49d6-46b6-85cc-8246c0f362bc', 'otros', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false);
 
+-- Inserción de Funkos en cada categoría
+INSERT INTO "funkos" ("nombre", "precio", "cantidad", "categoria_id", "created_at", "updated_at", "is_deleted")
+VALUES
+    ('mickey mouse', 20.0, 10, 'd69cf3db-b77d-4181-b3cd-5ca8107fb6a9', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+    ('iron man', 25.0, 8, '6dbcbf5e-8e1c-47cc-8578-7b0a33ebc154', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+    ('spider-man', 18.0, 12, '9def16db-362b-44c4-9fc9-77117758b5b0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+    ('funko genérico', 15.0, 15, '8c5c06ba-49d6-46b6-85cc-8246c0f362bc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false);
 DROP TABLE IF EXISTS "user_roles";
 DROP SEQUENCE IF EXISTS user_roles_id_seq;
 CREATE SEQUENCE user_roles_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 6 CACHE 1;
@@ -39,6 +53,13 @@ CREATE TABLE "public"."user_roles" (
                                        "id" integer DEFAULT nextval('user_roles_id_seq') NOT NULL,
                                        CONSTRAINT "PK_8acd5cf26ebd158416f477de799" PRIMARY KEY ("id")
 ) WITH (oids = false);
+
+INSERT INTO "user_roles" ("user_id", "role", "id") VALUES
+                                                       (1,	'USER',	1),
+                                                       (1,	'ADMIN',	2),
+                                                       (2,	'USER',	3),
+                                                       (3,	'USER',	4),
+                                                       (4,	'USER',	5);
 
 DROP TABLE IF EXISTS "usuarios";
 DROP SEQUENCE IF EXISTS usuarios_id_seq;
@@ -58,32 +79,10 @@ CREATE TABLE "public"."usuarios" (
                                      CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id"),
                                      CONSTRAINT "usuarios_username_key" UNIQUE ("username")
 ) WITH (oids = false);
-ALTER TABLE ONLY "public"."user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY (user_id) REFERENCES usuarios(id) NOT DEFERRABLE;
--- Inserción de categorías
-INSERT INTO "categorias" ("id", "categoria", "created_at", "updated_at", "is_deleted")
-VALUES
-    ('d69cf3db-b77d-4181-b3cd-5ca8107fb6a9', 'disney', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
-    ('6dbcbf5e-8e1c-47cc-8578-7b0a33ebc154', 'marvel', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
-    ('9def16db-362b-44c4-9fc9-77117758b5b0', 'superheroe', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
-    ('8c5c06ba-49d6-46b6-85cc-8246c0f362bc', 'otros', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false);
 
--- Inserción de Funkos en cada categoría
-INSERT INTO "funkos" ("nombre", "precio", "cantidad", "categoria_id", "created_at", "updated_at", "is_deleted")
-VALUES
-    ('mickey mouse', 20.0, 10, 'd69cf3db-b77d-4181-b3cd-5ca8107fb6a9', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
-    ('iron man', 25.0, 8, '6dbcbf5e-8e1c-47cc-8578-7b0a33ebc154', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
-    ('spider-man', 18.0, 12, '9def16db-362b-44c4-9fc9-77117758b5b0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
-    ('funko genérico', 15.0, 15, '8c5c06ba-49d6-46b6-85cc-8246c0f362bc', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false);
--- Inserción de User Roles
-INSERT INTO "user_roles" ("user_id", "role", "id") VALUES
-                                                       (1,	'USER',	1),
-                                                       (1,	'ADMIN',	2),
-                                                       (2,	'USER',	3),
-                                                       (3,	'USER',	4),
-                                                       (4,	'USER',	5);
---Inserción de Usuarios
 INSERT INTO "usuarios" ("is_deleted", "created_at", "id", "updated_at", "apellidos", "email", "nombre", "password", "username") VALUES
                                                                                                                                     ('f',	'2023-11-02 11:43:24.724871',	1,	'2023-11-02 11:43:24.724871',	'Admin Admin',	'admin@prueba.net',	'Admin',	'$2a$10$vPaqZvZkz6jhb7U7k/V/v.5vprfNdOnh4sxi/qpPRkYTzPmFlI9p2',	'admin'),
                                                                                                                                     ('f',	'2023-11-02 11:43:24.730431',	2,	'2023-11-02 11:43:24.730431',	'User User',	'user@prueba.net',	'User',	'$2a$12$RUq2ScW1Kiizu5K4gKoK4OTz80.DWaruhdyfi2lZCB.KeuXTBh0S.',	'user'),
                                                                                                                                     ('f',	'2023-11-02 11:43:24.733552',	3,	'2023-11-02 11:43:24.733552',	'Test Test',	'test@prueba.net',	'Test',	'$2a$10$Pd1yyq2NowcsDf4Cpf/ZXObYFkcycswqHAqBndE1wWJvYwRxlb.Pu',	'test'),
                                                                                                                                     ('f',	'2023-11-02 11:43:24.736674',	4,	'2023-11-02 11:43:24.736674',	'Otro Otro',	'otro@prueba.net',	'otro',	'$2a$12$3Q4.UZbvBMBEvIwwjGEjae/zrIr6S50NusUlBcCNmBd2382eyU0bS',	'otro');
+ALTER TABLE ONLY "public"."user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY (user_id) REFERENCES usuarios(id) NOT DEFERRABLE;
